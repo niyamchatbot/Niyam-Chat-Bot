8812e9f8b934f9e9e0c2173cb540b4507b9e19e5
niyamchatbot <niamyt2023@gmail.com>
2025-09-15 00:01:53 +0600

    Update bot.js

--- Script/commands/bot.js
+++ Script/commands/bot.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "Obot",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "⚠️ 𝐍𝐈𝐘𝐀𝐌 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️",
   description: "goibot",
   commandCategory: "Noprefix",
   usages: "noprefix",
